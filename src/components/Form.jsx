f6e2ce1bbe49f09c00102a959199fa81b9ba126a
alejandrourbina <urbinagonzalezja@gmail.com>
2021-02-21 16:03:08 +0100

    add form with inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,16 +3,49 @@ import React from 'react';
 const Form = () => {
     return ( 
         <>
-    <h2>Pedir Cita</h2>
+        <h2>Pedir Cita</h2>
 
         <form>
             <label>Nombre de tú Mascota</label>
-                <input
-                    type='text'
-                    name='mascota'
-                    className='u-full-width'
-                    placeholder='nombre mascota'
-                />
+            <input
+                type='text'
+                name='mascota'
+                className='u-full-width'
+                placeholder='Nombre mascota'
+            /> 
+
+            <label>Nombre del Dueño</label>
+            <input
+                type='text'
+                name='propietario'
+                className='u-full-width'
+                placeholder='Nombre del dueño'
+            />  
+
+            <label>Fecha</label>
+            <input
+                type='date'
+                name='fecha'
+                className='u-full-width'
+            />
+
+            <label>Hora</label>
+            <input
+                type='time'
+                name='hora'
+                className='u-full-width'
+            />
+            
+            <label>Síntomas</label>
+            <textarea
+                className='u-full-width'
+                name='síntomas'
+            ></textarea>
+
+            <button
+                type='submit'
+                className='u-full-width button'
+            >Agregar Cita</button>          
         </form>
         </>
      );
