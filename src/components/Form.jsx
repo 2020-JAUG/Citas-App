d4f7d993cd150670b7e86f037fd16e142fde8f9a
alejandrourbina <urbinagonzalezja@gmail.com>
2021-02-21 14:18:20 +0100

    add input to files form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Form = () => {
-    return (  );
+    return ( 
+        <>
+    <h2>Pedir Cita</h2>
+
+        <form>
+            <label>Nombre de tú Mascota</label>
+                <input
+                    type='text'
+                    name='mascota'
+                    className='u-full-width'
+                    placeholder='nombre mascota'
+                />
+        </form>
+        </>
+     );
 }
  
 export default Form;
